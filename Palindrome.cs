109cdcf8df87f5e68ee4d202f6ace379a66ba904
Anthony Balletta <anthonyballetta@gmail.com>
2017-10-17 15:38:30 -0400

    Adding palindrome solution

---
+++ Palindrome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string word = "otto";
+
+            if(checkPalindrome(word))
+            {
+                Console.WriteLine("This word is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("This word is NOT a palindrome");
+            }
+            Console.Read();
+        }
+
+       public static bool checkPalindrome(string inputString)
+        {
+            int length = inputString.Length;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (inputString[i] != inputString[length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
