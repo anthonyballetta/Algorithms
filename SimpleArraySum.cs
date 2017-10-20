5924d3d4a6fdbfdde00358004c62ef09c05475de
Anthony Balletta <anthonyballetta@gmail.com>
2017-10-20 18:51:09 -0400

    Adding SimpleArraySum.cs

---
+++ SimpleArraySum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class SimpleArraySum
+    {
+        static int simpleArraySum(int n, int[] ar)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum = sum + ar[i];
+            }
+            return sum;
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = simpleArraySum(n, ar);
+            Console.WriteLine(result);
+        }
+    }
+}
