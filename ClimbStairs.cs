f58457f2eaffaf5f4553a507830bccb5dcb861e1
Anthony Balletta <anthonyballetta@gmail.com>
2017-10-20 14:32:17 -0400

    Adding ClimbStairs.cs

---
+++ ClimbStairs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class ClimbStairs
+    {
+        public int climbSomeStairs(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            int first = 1;
+            int second = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                int third = first + second;
+                first = second;
+                second = third;
+            }
+            return second;
+        }
+    }
+}
