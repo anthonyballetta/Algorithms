a4fc2125536217d6b3910a763c4edf4f822d83a6
Anthony Balletta <anthonyballetta@gmail.com>
2017-10-23 15:17:13 -0400

    Adding SingleNumber.cs

---
+++ SingleNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class SingleNumber
+    {
+        class Solution
+        {
+            public static void Main(String[] args)
+            {
+                Solution s1 = new Solution();
+
+                int[] a = { 1, 1, 2, 2, 3 };
+
+                int answer = s1.singleNumber(a);
+                Console.WriteLine("test");
+                Console.WriteLine(answer.ToString());
+
+                Console.Read();
+            }
+
+            public int singleNumber(int[] A)
+            {
+                int result = A[0];
+                for (int i = 1; i < A.Length; i++)
+                {
+                    result = result ^ A[i];  /* Get the xor of all elements */
+                }
+                return result;
+            }
+        }
+    }
+}
