7f5e137d8f826461ac383cfa248ccbbdf7515a61
Anthony Balletta <anthonyballetta@gmail.com>
2017-10-24 16:20:56 -0400

    Adding DeleteNodeLinkedList.cs

---
+++ DeleteNodeLinkedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class DeleteNodeLinkedList
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
