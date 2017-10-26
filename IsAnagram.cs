053032a696aec4d387515bbadef255e4f1ec838a
Anthony Balletta <anthonyballetta@gmail.com>
2017-10-26 14:58:13 -0400

    Adding IsAnagram.cs

---
+++ IsAnagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class IsAnagram
+    {
+        public class Solution
+        {
+            public static void Main(String[] args)
+            {
+                bool answer = IsAnagram("apple","appel");
+                Console.WriteLine(answer);
+                Console.Read();
+            }
+
+            public static bool IsAnagram(string s, string t)
+            {
+                char[] firstWord = s.ToCharArray();
+                char[] secondWord = t.ToCharArray();
+                Array.Sort(firstWord);
+                Array.Sort(secondWord);
+                if (firstWord.Length == 0 && secondWord.Length == 0)
+                {
+                    return true;
+                }
+                return firstWord.SequenceEqual(secondWord);
+            }
+        }
+    }
+}
